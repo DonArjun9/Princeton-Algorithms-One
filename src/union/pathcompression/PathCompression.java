8d563743e4bf4697e24778389650f4d1cb8ff3c0
arjun soma <arjun.soma@powerschool.com>
2018-09-07 14:31:00 -0700

    pathcompression

---
+++ src/union/pathcompression/PathCompression.java
@@ -0,0 +1,50 @@
+package union.pathcompression;
+
+public class PathCompression {
+	
+	public int[] id;
+	public int[] size;
+	public int i;
+
+	public PathCompression(int n) {
+		i = n;
+		id = new int[n];
+		size = new int[n];
+		for (int i = 0; i < n; i++) {
+			id[i]=i;
+			size[i]=1;
+		
+	}
+	}
+	
+	public int root(int i) {
+		while(i != id[i]) {
+			i = id[id[i]];  //similar to WeightedQuickUnion except this line
+		}
+		return i;	
+	}
+	
+	public void union(int p, int q) {
+		int i =root(p);
+		int j =root(q);
+		if(i == j) {
+			return;
+		}
+		if(i < j) {
+			id[i] = j;
+			size[j] = size[i]+1;
+			
+		}else {
+			id[j] = i;
+			size[i] = size[j]+1;
+			
+		}
+		
+	}
+	
+	public boolean connected(int p, int q) {
+		if(root(p)==root(q)) {return true;}
+		return false;
+		
+	}
+}
